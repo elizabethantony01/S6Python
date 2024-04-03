df517f60fed1f5c809fa96d75c8d7052041f10e9
elizabethantony01 <161739644+elizabethantony01@users.noreply.github.com>
2024-04-04 01:18:58 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -8,7 +8,7 @@ run = True
 # main loop
 while run:
     root = Tk()
-    root.geometry('905x700')
+    #root.geometry('500x500')
     root.title('HANG MAN')
     root.config(bg = '#E7FFFF')
     count = 0
@@ -27,22 +27,31 @@ while run:
         exec('d{}.place(x={},y={})'.format(i,x,450))
         
     #letters icon
-    al = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for i in range(ord('A'), ord('Z')+1):
+        letter = chr(i)
+        #button = tk.Button(root, text=letter, command=lambda l=letter: button_click(l))
+        exec('button_{}= Button(root,bd=0,text=letter,command=lambda:check("button_{}","{}"),bg="#E7FFFF",activebackground="#E7FFFF",font=10)'.format(chr(i),chr(i),chr(i),chr(i),))
+        exec('button_{}.pack()'.format(chr(i)))
+    
+
+        
+    '''al = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     for let in al:
-        exec('{}=PhotoImage(file="{}.png")'.format(let,let))
+        exec('{}=PhotoImage(file="{}.png")'.format(let,let))'''
+    
+     #letters placement
+    '''
+    for q1 in button:
+        exec('{}=Button(root,bd=0,command=lambda:check("{}","{}"),bg="#E7FFFF",activebackground="#E7FFFF",font=10,image={})'.format(q1[0],q1[1],q1[0],q1[1]))
+        exec('{}.place(x={},y={})'.format(q1[0],q1[2],q1[3]))'''
+        
         
     # hangman images
     h123 = ['h1','h2','h3','h4','h5','h6','h7']
     for hangman in h123:
         exec('{}=PhotoImage(file="{}.png")'.format(hangman,hangman))
         
-    #letters placement
-    button = [['b1','a',0,595],['b2','b',70,595],['b3','c',140,595],['b4','d',210,595],['b5','e',280,595],['b6','f',350,595],['b7','g',420,595],['b8','h',490,595],['b9','i',560,595],['b10','j',630,595],['b11','k',700,595],['b12','l',770,595],['b13','m',840,595],['b14','n',0,645],['b15','o',70,645],['b16','p',140,645],['b17','q',210,645],['b18','r',280,645],['b19','s',350,645],['b20','t',420,645],['b21','u',490,645],['b22','v',560,645],['b23','w',630,645],['b24','x',700,645],['b25','y',770,645],['b26','z',840,645]]
-
-    for q1 in button:
-        exec('{}=Button(root,bd=0,command=lambda:check("{}","{}"),bg="#E7FFFF",activebackground="#E7FFFF",font=10,image={})'.format(q1[0],q1[1],q1[0],q1[1]))
-        exec('{}.place(x={},y={})'.format(q1[0],q1[2],q1[3]))
-        
+   
     #hangman placement
     han = [['c1','h1'],['c2','h2'],['c3','h3'],['c4','h4'],['c5','h5'],['c6','h6'],['c7','h7']]
     for p1 in han:
@@ -98,3 +107,4 @@ while run:
                     run = False
                     root.destroy()         
     root.mainloop()
+
